de437ccdb470be02defb76d1bb4998a710c59ff8
Rich Townsend <devnull@localhost>
2013-10-24 15:19:04 -0500

    Updated testing framework to more-flexible per-test scripts

---
+++ test/nad/mesa/bcep/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# File     : gyre.sh
+# Purpose  : GYRE testing script
+
+. test_support
+
+# Settings
+
+EXEC=./gyre_nad
+
+IN_FILE=gyre_nad.in
+OUT_FILE=gyre_nad.txt
+
+LABEL="MESA model for beta Cephei star"
+
+RELERR=1E-15
+FIELDS=1-5
+
+# Do the tests
+
+run_gyre $EXEC $IN_FILE "$LABEL"
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
+
+check_output $OUT_FILE $RELERR $FIELDS
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
+
+# Clean up output files
+
+rm -f *.txt
+
+# Finish
+
+echo " ...succeeded"
