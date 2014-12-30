624e1e86df04f9d8ad25b7712dd25b3b2da7a435
Rich Townsend <devnull@localhost>
2014-12-29 19:14:01 -0600

    Updated tests

--- test/ad/poly/4.97/test.sh
+++ test/ad/poly/4.97/test.sh
@@ -12,7 +12,7 @@ EXEC=./gyre_ad
 IN_FILE=gyre_ad.in
 OUT_FILE=gyre_ad.txt
 
-LABEL="homogeneous compressible model (analytic structure)"
+LABEL="homogeneous compressible model (polytropic structure)"
 
 RELERR=3E-14
 FIELDS=1-5
