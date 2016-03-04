9648ba4d1f3e640cf09d5b45574e8bd8fbc83f0c
Rich Townsend <devnull@localhost>
2016-03-03 22:33:38 -0500

    Updated tests

---
+++ test/ad/poly/3.0+3.0/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# File     : test.sh
+# Purpose  : GYRE testing script
+
+. test_support
+
+# Settings
+
+EXEC=./gyre
+
+IN_FILE=gyre.in
+OUT_FILE=summary.txt
+
+LABEL="polytrope model (n_poly=3.0 w/ double point)"
+
+ABSERR=1E-6
+FIELDS=1-2,5
+
+# Do the tests
+
+run_gyre $EXEC $IN_FILE "$LABEL"
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
+
+check_output $ABSERR $FIELDS $OUT_FILE '' abs
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
+
+# Clean up output files
+
+rm -f $OUT_FILE
+
+# Finish
+
+echo " ...succeeded"
