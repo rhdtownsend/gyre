ef503ef25eb9feee6496595a30e804627c0e377f
Rich Townsend <devnull@localhost>
2017-02-04 20:54:22 -0600

    Added diff-* tests

---
+++ test/ad/num/diff-magnus/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# File     : test.sh
+# Purpose  : GYRE testing script
+
+. test_support
+
+# Settings
+
+EXEC=./gyre
+
+IN_FILE=gyre.in
+OUT_FILE=summary.txt
+
+LABEL="numerics (Magnus difference schemes)"
+
+RELERR=3E-13
+FIELDS=1-6
+
+# Do the tests
+
+run_gyre $EXEC $IN_FILE "$LABEL"
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
+
+check_output $RELERR $FIELDS $OUT_FILE
+if [ $? -ne 0 ]; then
+    exit 1;
+fi
+
+# Clean up output files
+
+rm -f $OUT_FILE
+
+# Finish
+
+echo " ...succeeded"
