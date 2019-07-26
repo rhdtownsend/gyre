rhdtownsend/gyre

commit 627f8501669854d0263b6db3972dee963295a13e

Added check for degenerate grids in grid_t constructor
This verifies that there are at least two points in each segment. If not,
it ABORTS with an error.

src/grid/gyre_grid.fpp
@@ -89,6 +89,7 @@ contains
     type(grid_t)         :: gr
 
     integer :: n_k
+    integer :: s
     integer :: k
 
     ! Construct a grid_t from the input abscissae x (with segment
@@ -102,24 +103,31 @@ contains
        
        allocate(gr%pt(n_k))
 
+       s = 1
+
        gr%pt(1)%x = x(1)
-       gr%pt(1)%s = 1
+       gr%pt(1)%s = s
 
-       do k = 2, n_k
-
-          gr%pt(k)%x = x(k)
+       do k = 1, n_k
 
           if (x(k) == x(k-1)) then
-             gr%pt(k)%s = gr%pt(k-1)%s + 1
-          else
-             gr%pt(k)%s = gr%pt(k-1)%s
+             s = s + 1
           endif
+          
+          gr%pt(k)%x = x(k)
+          gr%pt(k)%s = s
 
        end do
 
     end if
 
     gr%n_k = n_k
+
+    ! Check for degenerate segments
+
+    check_loop : do s = 1, gr%pt(n_k)%s
+       $ASSERT(COUNT(gr%pt%s == s) >= 2,Degenerate segment)
+    end do check_loop
 
     ! Finish
 

commit b1d2cddb8f26b1291f5d43a1a84e14aeb26b2087

Fixed bug introduced in 627f8501

src/grid/gyre_grid.fpp
@@ -108,7 +108,7 @@ contains
        gr%pt(1)%x = x(1)
        gr%pt(1)%s = s
 
-       do k = 1, n_k
+       do k = 2, n_k
 
           if (x(k) == x(k-1)) then
              s = s + 1