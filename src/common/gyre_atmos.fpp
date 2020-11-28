rhdtownsend/gyre

commit a22648845dd42f63f61dfc6992734ceca4d9997a

Renamed outer_bound_for_cutoff to outer_bound_cutoff, and outer_branch to outer_bound_branch

src/common/gyre_atmos.fpp
@@ -113,14 +113,14 @@ contains
 
   function atmos_chi_c_ (V, As, c_1, Gamma_1, omega, lambda, branch) result (chi)
 
-    real(WP), intent(in)               :: V
-    real(WP), intent(in)               :: As
-    real(WP), intent(in)               :: c_1
-    real(WP), intent(in)               :: Gamma_1
-    complex(WP), intent(in)            :: omega
-    complex(WP), intent(in)            :: lambda
-    character(*), intent(in), optional :: branch
-    complex(WP)                        :: chi
+    real(WP), intent(in)     :: V
+    real(WP), intent(in)     :: As
+    real(WP), intent(in)     :: c_1
+    real(WP), intent(in)     :: Gamma_1
+    complex(WP), intent(in)  :: omega
+    complex(WP), intent(in)  :: lambda
+    character(*), intent(in) :: branch
+    complex(WP)              :: chi
 
     character(:), allocatable :: branch_
     complex(WP)               :: a_11
@@ -131,12 +131,6 @@ contains
     complex(WP)               :: c
     complex(WP)               :: psi2
     complex(WP)               :: psi
-
-    if (PRESENT(branch)) then
-       branch_ = branch
-    else
-       branch_ = 'E_NEG'
-    endif
 
     ! Calculate the atmospheric radial wavenumber (complex frequencies)
 

src/common/gyre_freq_context.fpp
@@ -197,8 +197,8 @@ contains
 
     ! Evaluate the cutoff frequencies
 
-    if (os_p%outer_bound_for_cutoff /= '') then
-       outer_bound = os_p%outer_bound_for_cutoff
+    if (os_p%outer_bound_cutoff /= '') then
+       outer_bound = os_p%outer_bound_cutoff
     else
        outer_bound = os_p%outer_bound
     endif

src/hdf/gyre_hgroup.inc
@@ -1,0 +1,37 @@
+! Incfile  : gyre_hgroup
+! Purpose  : macros for gyre_hgroup module
+!
+! Copyright 2020 Rich Townsend
+!
+! This file is part of GYRE. GYRE is free software: you can
+! redistribute it and/or modify it under the terms of the GNU General
+! Public License as published by the Free Software Foundation, version 3.
+!
+! GYRE is distributed in the hope that it will be useful, but WITHOUT
+! ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+! or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+! License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+$perl
+xmacro 'HDF5_CALL', sub {
+  my @a = @_; @a = get_args() unless @a;
+  die("Invalid number of arguments:".scalar @a) unless scalar @a >= 1;
+  my $proc_name = shift @a;
+  @pos_args = grep(!/=/, @a);
+  @key_args = grep(/=/, @a);
+  @args = (@pos_args, 'hdf_err', @key_args);
+  my $arg_list = join(', ', @args);
+  return <<EOF;
+call $proc_name($arg_list)
+if(hdf_err == -1) then
+   call h5eprint_f (hdf_err)
+   write(UNIT=ERROR_UNIT, FMT=*) 'ABORT at line $line_num $context_doc:'
+   write(UNIT=ERROR_UNIT, FMT=*) 'Error in call to $proc_name'
+   stop 'Program aborted'
+endif
+EOF
+};
+$endperl

src/nad/gyre_nad_bound.fpp
@@ -191,7 +191,7 @@ contains
        $ABORT(Invalid time_factor)
     end select
 
-    bd%branch_o = os_p%outer_branch
+    bd%branch_o = os_p%outer_bound_branch
 
     call bd%stencil_(pt_i, pt_o)
 

src/par/gyre_osc_par.fpp
@@ -47,8 +47,8 @@ module gyre_osc_par
      character(64)           :: variables_set = 'GYRE'
      character(64)           :: inner_bound = 'REGULAR'
      character(64)           :: outer_bound = 'VACUUM'
-     character(64)           :: outer_bound_for_cutoff = 'UNNO'
-     character(64)           :: outer_branch = 'E_NEG'
+     character(64)           :: outer_bound_cutoff = ''
+     character(64)           :: outer_bound_branch = 'E_NEG'
      character(64)           :: inertia_norm = 'BOTH'
      character(64)           :: time_factor = 'OSC'
      character(64)           :: conv_scheme = 'FROZEN_PESNELL_1'
@@ -117,8 +117,8 @@ contains
     character(LEN(os_p%variables_set))    :: variables_set
     character(LEN(os_p%inner_bound))      :: inner_bound
     character(LEN(os_p%outer_bound))      :: outer_bound
-    character(LEN(os_p%outer_bound))      :: outer_bound_for_cutoff 
-    character(LEN(os_p%outer_branch))     :: outer_branch
+    character(LEN(os_p%outer_bound))      :: outer_bound_cutoff 
+    character(LEN(os_p%outer_bound))      :: outer_bound_branch
     character(LEN(os_p%inertia_norm))     :: inertia_norm
     character(LEN(os_p%time_factor))      :: time_factor
     character(LEN(os_p%conv_scheme))      :: conv_scheme
@@ -133,9 +133,10 @@ contains
     logical                               :: eddington_approx
     logical                               :: reduce_order
 
-    namelist /osc/ x_ref, x_atm, alpha_grv, alpha_thm, alpha_hfl, alpha_gam, alpha_pi, alpha_kap, &
-         eps_rho, eps_T, inner_bound, outer_bound, &
-         outer_bound_for_cutoff, outer_branch, variables_set, inertia_norm, time_factor, &
+    namelist /osc/ x_ref, x_atm, alpha_grv, alpha_thm, alpha_hfl, &
+         alpha_gam, alpha_pi, alpha_kap, &
+         eps_rho, eps_T, inner_bound, outer_bound, outer_bound_cutoff, outer_bound_branch, &
+         variables_set, inertia_norm, time_factor, &
          conv_scheme, zeta_scheme, deps_source, deps_file, deps_file_format, &
          tag_list, adiabatic, nonadiabatic, quasiad_eigfuncs, &
          eddington_approx, reduce_order
@@ -178,8 +179,8 @@ contains
        variables_set = os_p(i)%variables_set
        inner_bound = os_p(i)%inner_bound
        outer_bound = os_p(i)%outer_bound
-       outer_bound_for_cutoff = os_p(i)%outer_bound_for_cutoff
-       outer_branch = os_p(i)%outer_branch
+       outer_bound_cutoff = os_p(i)%outer_bound_cutoff
+       outer_bound_branch = os_p(i)%outer_bound_branch
        inertia_norm = os_p(i)%inertia_norm
        time_factor = os_p(i)%time_factor
        conv_scheme = os_p(i)%conv_scheme
@@ -213,8 +214,8 @@ contains
        os_p(i)%variables_set = variables_set
        os_p(i)%inner_bound = inner_bound
        os_p(i)%outer_bound = outer_bound
-       os_p(i)%outer_bound_for_cutoff = outer_bound_for_cutoff
-       os_p(i)%outer_branch = outer_branch
+       os_p(i)%outer_bound_cutoff = outer_bound_cutoff
+       os_p(i)%outer_bound_branch = outer_bound_branch
        os_p(i)%inertia_norm = inertia_norm
        os_p(i)%time_factor = time_factor
        os_p(i)%conv_scheme = conv_scheme
@@ -262,7 +263,8 @@ contains
     call bcast(os_p%variables_set, root_rank)
     call bcast(os_p%inner_bound, root_rank)
     call bcast(os_p%outer_bound, root_rank)
-    call bcast(os_p%outer_bound_for_cutoff, root_rank)
+    call bcast(os_p%outer_bound_cutoff, root_rank)
+    call bcast(os_p%outer_bound_branch, root_rank)
     call bcast(os_p%inertia_norm, root_rank)
     call bcast(os_p%time_factor, root_rank)
     call bcast(os_p%conv_scheme, root_rank)

commit 417df584773b1b5a2509bdbb8f03816a10acdc8a

Fixed bug introduced in a226488, involving not using the passed branch parameter

src/common/gyre_atmos.fpp
@@ -122,15 +122,14 @@ contains
     character(*), intent(in) :: branch
     complex(WP)              :: chi
 
-    character(:), allocatable :: branch_
-    complex(WP)               :: a_11
-    complex(WP)               :: a_12
-    complex(WP)               :: a_21
-    complex(WP)               :: a_22
-    complex(WP)               :: b
-    complex(WP)               :: c
-    complex(WP)               :: psi2
-    complex(WP)               :: psi
+    complex(WP) :: a_11
+    complex(WP) :: a_12
+    complex(WP) :: a_21
+    complex(WP) :: a_22
+    complex(WP) :: b
+    complex(WP) :: c
+    complex(WP) :: psi2
+    complex(WP) :: psi
 
     ! Calculate the atmospheric radial wavenumber (complex frequencies)
 
@@ -147,7 +146,7 @@ contains
 
     ! Adjust the sign of psi to choose the correct solution branch
 
-    select case (branch_)
+    select case (branch)
 
     case ('E_POS')
 