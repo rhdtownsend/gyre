5e81a7562905b2d07c179614265a3b2c63769206
Rich Townsend <townsend@astro.wisc.edu>
2026-03-04 16:35:05 -0600

    Added stress tests

---
+++ test/stress/stress.diff_scheme/run_stress_test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import subprocess
+import tempfile
+import astropy.table as at
+import astropy.io as ai
+import numpy as np
+import time
+
+# Parameters
+
+tmpl_file = sys.argv[1]
+n_reps = int(sys.argv[2])
+
+# Read the template file
+
+with open(tmpl_file, 'r') as f:
+    template = f.read()
+
+# Define run variables
+
+DIFF_SCHEMES = ['COLLOC_GL2', 'COLLOC_GL4', 'COLLOC_GL6',
+                'MAGNUS_GL2', 'MAGNUS_GL4', 'MAGNUS_GL6',
+                'TRAPZ', 'MIRK']
+
+ORDERS = {
+    'COLLOC_GL2': 2,
+    'COLLOC_GL4': 4,
+    'COLLOC_GL6': 6,
+    'MAGNUS_GL2': 2,
+    'MAGNUS_GL4': 4,
+    'MAGNUS_GL6': 6,
+    'TRAPZ': 2,
+    'MIRK': 4}
+
+# Loop through run variables, storing timing data in table
+
+tbl = at.Table(names=('diff_scheme', 'order', 'runtime'),
+               dtype=(str, int, float))
+
+for diff_scheme in DIFF_SCHEMES:
+
+    with tempfile.NamedTemporaryFile(mode='w+t') as f:
+
+        # Create the input file
+
+        f.write(template.format(diff_scheme=diff_scheme))
+        f.flush()
+
+        # Loop over repititions
+
+        times = []
+
+        for i in range(n_reps):
+
+            # Run GYRE, capturing output
+
+            start_time = time.perf_counter()
+
+            try:
+                result = subprocess.run(('./gyre', f'{f.name}'), capture_output=True, text=True, check=True)
+            except subprocess.CalledProcessError as e:
+                print('gyre failed with error', e.stderr)
+
+            end_time = time.perf_counter()
+
+            times += [end_time - start_time]
+
+        tbl.add_row([diff_scheme, ORDERS[diff_scheme], np.mean(times)])
+
+# Print out results
+        
+for col in tbl.itercols():
+    if col.info.dtype.kind == 'f':
+        col.info.format = '.2f'
+        
+ai.ascii.write(tbl, sys.stdout, format='rst')
+
+
